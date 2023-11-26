0314c0026e4603c12eabe0726eaad4466c0d3c59
Ornel Kevin Piam Talom <ornel.kevin.piam.talom@mni.thm.de>
2023-11-26 15:47:57 +0100

    Test: Check if index.php is rendered>>

--- public/index.php
+++ public/index.php
@@ -1,9 +1,2 @@
 <?php
-
-use  okpt\my\web\page\Kernel;
-
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+phpinfo();
