b49ead0bbd58cc3126c7433b045c8e2fdb5b32e5
Ornel Kevin Piam Talom <ornel.kevin.piam.talom@mni.thm.de>
2023-12-08 07:22:07 +0100

    Bug: Modifie the home route

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,6 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
+    #[Route('', name: 'app_index')]
+    public function indexInit(): Response
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
     #[Route('/index', name: 'app_index')]
     public function index(): Response
     {
